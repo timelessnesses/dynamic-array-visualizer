552f4daa94fb1e795e3c59905a590dd0254c09a4
timelessnesses <timelessnesses@timelessnesses.me>
2025-02-23 13:11:47 +0700

    hooray

---
+++ start.bat
@@ -0,0 +1,7 @@
+start "1.1 Growth Factor" /d . cmd /C cargo run -r -- 1.1
+start "1.5 Growth Factor" /d . cmd /C cargo run -r -- 1.5
+start "1.125 Growth Factor" /d . cmd /C cargo run -r -- 1.125
+start "2 Growth Factor" /d . cmd /C cargo run -r -- 2.0
+start "1.25 Growth Factor" /d . cmd /C cargo run -r -- 1.25
+start "Phi Growth Factor" /d . cmd /C cargo run -r -- 1.61803398875
+start "E (Log Natural) Growth Factor" /d . cmd /C cargo run -r -- 2.71828182846
